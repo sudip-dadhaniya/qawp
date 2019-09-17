3a1305b029b8c27318c9350806d8074044dac9e8
Sudip <sudip.dadhaniya@multidots.com>
2019-09-17 19:35:47 +0530

    lamda

---
+++ wp-content/plugins/lambdatest-screenshot/lt_scr_lambdatest_config.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+$lt_falcon_url = "https://manual-api.lambdatest.com";
+$lt_lums_url = "https://accounts.lambdatest.com";
+$download_url = "https://app.lambdatest.com";
