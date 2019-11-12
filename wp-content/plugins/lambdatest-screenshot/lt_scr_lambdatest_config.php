b382339d5e2959da2a4a08bcbccbcec498b9ceb8
Sudip <sudip.dadhaniya@multidots.com>
2019-11-12 14:38:19 +0530

    remove

--- wp-content/plugins/lambdatest-screenshot/lt_scr_lambdatest_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined('ABSPATH')) {
-    exit;
-}
-$lt_falcon_url = "https://manual-api.lambdatest.com";
-$lt_lums_url = "https://accounts.lambdatest.com";
-$download_url = "https://app.lambdatest.com";
--- wp-content/themes/wp-store/inc/js/admin.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-    
-    $('#upload-btn').click(function(){
-		$('#optionsframework form').attr('action','');
-	});
-    
-  
-});
